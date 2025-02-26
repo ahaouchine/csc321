2dcf5f9919302a24790c021a786c43b64d8b6fc2
ahaouchine1 <haouchineaziz3@gmail.com>
2025-02-26 02:20:10 +0000

    lab number 7

---
+++ ahaouchine7.py
@@ -0,0 +1,36 @@
+# aziz haouchine
+# csc-321
+# lab 7
+
+
+
+# Define variables
+
+x = 7  # Integer variable
+y = 2.5  # Float variable
+z = "us"  # String variable
+
+# Integer comparison
+if x > 5:
+    print("x is greater than 5")
+elif x == 5:
+    print("x is equal to 5")
+else:
+    print("x is less than 5")
+
+# Float comparison
+if y < 4.0:
+    print("y is less than 4.0")
+elif y == 4.0:
+    print("y is equal to 4.0")
+else:
+    print("y is greater than 4.0")
+
+# String comparison
+if z == "us":
+    print("z is exactly 'us'")
+elif z > "us":  # Lexicographic comparison
+    print("z comes after 'us' in dictionary order")
+else:
+    print("z comes before 'us' in dictionary order")
+("x is ")
