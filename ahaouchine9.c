8f6e6b57e493616e758e82772f289f3d13cd941d
ahaouchine1 <haouchineaziz3@gmail.com>
2025-04-18 09:35:56 +0000

    rewrite C and Java programs for resubmission

--- ahaouchine9.c
+++ ahaouchine9.c
@@ -4,51 +4,64 @@
 
 
 
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
-    int randomNumber = 3, userGuess;
-    
-    // Display the correct number before the user guesses
-    printf("Your correct number is %d\n\n", randomNumber);
-    
-    printf("Guess a number between 1 and 20:\n\n");
-    
-    while (1) {
+
+// Guess the number game in C
+int main(void) 
+{
+    int randomNumber, userGuess;
+
+    // Initialize random seed based on current time
+    srand(time(NULL));
+    randomNumber = (rand() % 20) + 1; 
+
+    // Prompt user to guess
+    printf("please make sure the number is between 1 and 20:\n\n");
+
+    while (1) 
+    {
         scanf("%d", &userGuess);
-        
-        // Check if the guess is out of range
-        if (userGuess < 1 || userGuess > 20) {
-            printf("You input a number that is out of range (1-20) re-enter a valid number.\n\n");
+
+        // Check if the number entred is out of valid range
+        if (userGuess < 1 || userGuess > 20)
+       	{
+            printf("You input a number that is out of range (1–20). Please try again:\n\n");
             continue;
         }
-        
-        // Check if the guess is correct
-        if (userGuess == randomNumber) {
+
+        // Correct guess
+        if (userGuess == randomNumber) 
+	{
+            printf("You won and guessed the number correctly!\n");
             for (int i = 0; i < userGuess * 2; i++) {
-                printf("You won and guessed the number correctly!\n");
+                printf(" Congrats! \n");
             }
+
             break;
-        } else {
-            // Inform user of loss and whether their guess was too high or too low
-            if (userGuess < randomNumber) {
-                printf("You lost and your guess is lower than my number which was %d.\n", randomNumber);
-            } else {
-                printf("You lost and your guess is higher than my number which was %d.\n", randomNumber);
+
+        }
+       	else
+       	{
+            // Incorrect guess and tell the user if it's higher or lower AND reveal the correct number
+            if (userGuess < randomNumber) 
+	    {
+            
+		    printf("You lost! Your guess is LOWER than the correct number, which was %d.\n", randomNumber);
+            }else
+	    {
+                printf("You lost! Your guess is HIGHER than the correct number, which was %d.\n", randomNumber);
             }
             break;
         }
     }
-    
+
     return EXIT_SUCCESS;
 }
-    
-   
-
-
-
 
 
 
--- ahaouchine9.java
+++ ahaouchine9.java
@@ -6,45 +6,62 @@
 
 
 import java.util.Scanner;
+import java.util.Random;
+
 
 public class GuessNumberGame {
+
     public static void main(String[] args) {
-        int randomNumber = 3;
-        int userGuess;
+
+        int randomNumber, userGuess;
+
         Scanner scanner = new Scanner(System.in);
+        Random rand = new Random();
 
-        // Display the correct number before the user guesses
-        System.out.println("Your correct number is " + randomNumber + "\n");
+        // Generate a random number between 1 and 20
+        randomNumber = rand.nextInt(20) + 1;
 
+        // Prompt the user to guess
         System.out.println("Guess a number between 1 and 20:\n");
 
         while (true) {
             userGuess = scanner.nextInt();
 
-            // Check if the guess is out of range
+            // Check if guess is out of range
             if (userGuess < 1 || userGuess > 20) {
-                System.out.println("You input a number that is out of range (1-20) re-enter a valid number.\n");
+                System.out.println("You input a number that is out of range (1–20). Please try again:\n");
                 continue;
             }
 
-            // Check if the guess is correct
+            // Correct guess
             if (userGuess == randomNumber) {
-                for (int i = 0; i < userGuess * 2; i++) {
-                    System.out.println("You won and guessed the number correctly!");
+                System.out.println("You won and guessed the number correctly!");
+                
+		for (int i = 0; i < userGuess * 2; i++) {
+                    System.out.println(" Congrats! ");
                 }
                 break;
-            } else {
-                // Inform user of loss and whether their guess was too high or too low
+            
+	    } else {
+                
+		    // Incorrect guess 
                 if (userGuess < randomNumber) {
-                    System.out.println("You lost and your guess is lower than my number which was " + randomNumber + ".");
-                } else {
-                    System.out.println("You lost and your guess is higher than my number which was " + randomNumber + ".");
+                    System.out.println("You lost, Your guess is LOWER than the correct number, which was " + randomNumber + ".");
+                
+		} else {
+                    System.out.println("You lost, Your guess is HIGHER than the correct number, which was " + randomNumber + ".");
                 }
                 break;
             }
         }
 
         scanner.close();
-
     }
 }
+
+
+
+
+
+
+
